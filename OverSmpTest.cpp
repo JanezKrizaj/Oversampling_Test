6078dd99f2dd9eac735d0457779d7f4fd584864b
John Crosby <33461940+JanezKrizaj@users.noreply.github.com>
2020-04-12 18:00:16 +0200

    Add files via upload

---
+++ OverSmpTest.cpp
@@ -0,0 +1,86 @@
+#include "OverSmpTest.h"
+#include "IPlug_include_in_plug_src.h"
+#include "IControls.h"
+
+OverSmpTest::OverSmpTest(const InstanceInfo& info)
+: Plugin(info, MakeConfig(kNumParams, kNumPrograms))
+{
+    GetParam(kMix)->InitDouble("Mix", 50., 0., 100., 1., "%");
+    GetParam(kPhase)->InitInt("Flip", 0, 0, 1, "∅");
+    GetParam(kDelay)->InitInt("Delay", 0, 0, 7, "smp");
+    GetParam(kOverSampling)->InitEnum("Oversample", 0, 5, "", 0, "", OVERSAMPLING_FACTORS_VA_LIST);
+#if IPLUG_EDITOR // http://bit.ly/2S64BDd
+    mMakeGraphicsFunc = [&]() {
+        return MakeGraphics(*this, PLUG_WIDTH, PLUG_HEIGHT, PLUG_FPS, 1.);
+    };
+    
+    mLayoutFunc = [&](IGraphics* pGraphics) {
+        pGraphics->AttachCornerResizer(EUIResizerMode::Scale, false);
+        pGraphics->AttachPanelBackground(COLOR_GRAY);
+        pGraphics->LoadFont("Roboto-Regular", ROBOTO_FN);
+        const IRECT b = pGraphics->GetBounds();
+        
+        pGraphics->AttachControl(new IVKnobControl(b.GetCentredInside(100).GetHShifted(-50).GetVShifted(-50), kMix));
+        pGraphics->AttachControl(new IVRadioButtonControl(b.GetCentredInside(100).GetHShifted(50).GetVShifted(-50), kPhase));
+        
+        pGraphics->AttachControl(new IVKnobControl(b.GetCentredInside(100).GetHShifted(-50).GetVShifted(50), kDelay));
+        pGraphics->AttachControl(new IVRadioButtonControl(b.GetCentredInside(100).GetHShifted(50).GetVShifted(50), kOverSampling));
+    };
+#endif
+}
+
+#if IPLUG_DSP
+void OverSmpTest::ProcessBlock(sample** inputs, sample** outputs, int nFrames)
+{
+    if(mFactorChanged)
+    {
+        mOverSampL.SetOverSampling((EFactor) GetParam(kOverSampling)->Int());
+        mOverSampR.SetOverSampling((EFactor) GetParam(kOverSampling)->Int()); // Warning, this could allocate.
+        mFactorChanged = false;
+    }
+    
+    for (auto s = 0; s < nFrames; s++) {
+        procs[0][0] = inputs[0][s];
+        procs[1][0] = inputs[1][s];
+        
+        outputs[0][s] = mix * (mOverSampL.Process(inputs[0][s], [](sample input) {return input;}))
+        + (1. - mix) * procs[0][delay] * (double)phase;
+        outputs[1][s] = mix * (mOverSampR.Process(inputs[1][s], [](sample input) {return input;}))
+        + (1. - mix) * procs[1][delay] * (double)phase;
+        
+        //buffer filler
+        for (int i = 0; i < 7; i++) {
+            procs[0][7 - i] = procs[0][6 - i];
+            procs[1][7 - i] = procs[1][6 - i];
+        }
+    }
+}
+#endif
+
+void OverSmpTest::OnReset()
+{
+    mOverSampL.Reset(GetBlockSize());
+    mOverSampR.Reset(GetBlockSize());
+}
+
+void OverSmpTest::OnParamChange(int paramIdx, EParamSource, int sampleOffset)
+{
+    switch (paramIdx)
+    {
+        case kOverSampling:
+            mFactorChanged = true; // TODO: maybe check existing factor
+            break;
+        case kMix:
+            mix = GetParam(kMix)->Value() * 0.01;
+            break;
+        case kPhase:
+            phase = 1 + GetParam(kPhase)->Value() * -2;
+            break;
+        case kDelay:
+            delay = GetParam(kDelay)->Value();
+            break;
+            
+        default:
+            break;
+    }
+}
---
+++ OverSmpTest.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "IPlug_include_in_plug_hdr.h"
+#include "Oversampler.h"
+const int kNumPrograms = 1;
+
+enum EParams
+{
+    kMix = 0,
+    kPhase,
+    kOverSampling,
+    kDelay,
+    kNumParams
+};
+
+using namespace iplug;
+using namespace igraphics;
+
+class OverSmpTest final : public Plugin
+{
+public:
+    OverSmpTest(const InstanceInfo& info);
+    
+#if IPLUG_DSP // http://bit.ly/2S64BDd
+    void ProcessBlock(sample** inputs, sample** outputs, int nFrames) override;
+    void OnReset() override;
+    void OnParamChange(int paramIdx, EParamSource, int sampleOffset) override;
+    
+private:
+    bool mFactorChanged = true;
+    OverSampler <sample> mOverSampL {kNone, true, 1};
+    OverSampler <sample> mOverSampR {kNone, true, 1}; // init with no upsampling, block processing, mono
+    double mix;
+    int delay, phase;
+      double procs[2][8];
+#endif
+};
